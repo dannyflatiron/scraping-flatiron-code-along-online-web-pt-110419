2676508794e482f8d1ff1e240aa76bb3bfd298b5
Daniel Reina <dannyflatiron@gmail.com>
2019-12-31 02:12:35 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -3,6 +3,7 @@ require 'open-uri'
 require 'pry'
 
 require_relative './course.rb'
+# we are requiring our Course class file so that our Scraper can make new courses and give them attributes scarped from the web page
 
 class Scraper
 
@@ -19,6 +20,14 @@ class Scraper
 
   def get_page
     doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
+
+    doc.css(".post").each do |post|
+      course = Course.new
+      course.title = post.css("h2").text
+      course.schedule = post.css(".date").text
+      course.description = post.css("p").text
+    end
+
 
     binding.pry
   end
