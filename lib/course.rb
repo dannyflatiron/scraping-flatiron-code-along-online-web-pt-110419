3a2f3adbb47a72d6b8e7f55a016db20f8e217077
Daniel Reina <dannyflatiron@gmail.com>
2019-12-31 01:24:33 +0000

    Automatically backed up by Learn

--- lib/course.rb
+++ lib/course.rb
@@ -2,6 +2,16 @@ class Course
 
 attr_accessor :title, :schedule, :description
 
-def initialize
+@@all = []
+
+def initialize(title, schedule, description)
+  @title = title
+  @schedule = schedule
+  @description = description
+  @@all << self
+end
+
+def self.all
+  @@all
 end
 end
